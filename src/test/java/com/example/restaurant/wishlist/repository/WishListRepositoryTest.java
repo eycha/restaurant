7accd7158a5b99ebdf217ad1e2bc94773a939da0
eycha <silvlexonst@gmail.com>
2023-03-26 20:50:37 +0900

    test: save test

---
+++ src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.restaurant.wishlist.repository;
+
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import com.example.restaurant.wishlist.entity.repository.WishListRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class WishListRepositoryTest {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    private WishListEntity create() {
+        var wishList = new WishListEntity();
+        wishList.setTitle("title");
+        wishList.setCategory("category");
+        wishList.setAddress("address");
+        wishList.setReadAddress("readAddress");
+        wishList.setHomePageLink("");
+        wishList.setImageLink("");
+        wishList.setVisit(false);
+        wishList.setVisitCount(0);
+        wishList.setLastVisitDate(null);
+
+        return wishList;
+
+    }
+
+    @Test
+    public void saveTest() {
+        var wishListEntity =create();
+        wishListRepository.save(wishListEntity);
+        var expected = wishListRepository.findById(1);
+
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1,expected.get().getIndex());
+
+    }
+    @Test
+    public void findByIdTest() {
+
+    }
+    @Test
+    public void deleteTest() {
+
+    }
+    @Test
+    public void listAllTest() {
+
+    }
+}
