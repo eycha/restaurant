6bee30faefd886aa976503d9fbb13fcdeb35a8d1
eycha <silvlexonst@gmail.com>
2023-03-28 23:09:04 +0900

    chore: naver client component

--- src/main/java/com/example/restaurant/naver/NaverClient.java
+++ src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -1,4 +1,26 @@
 package com.example.restaurant.naver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NaverClient {
+
+    @Value("&{naver.client.id}")
+    private String naverClientId;
+    @Value("&{naver.client.secret}")
+    private String naverScret;
+
+    @Value("&{naver.url.search.local}")
+    private String naverLocalSearchUrl;
+    @Value("&{naver.url.search.image}")
+    private String naverImageSearchUrl;
+
+    public void localSearch() {
+
+    }
+
+    public void imageSearch() {
+
+    }
 }
