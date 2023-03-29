b1393758e6804d877e1474e27cb960fb77091762
eycha <silvlexonst@gmail.com>
2023-03-29 23:36:06 +0900

    chore: Search Local Response

---
+++ src/main/java/com/example/restaurant/naver/dto/SearchLocalRes.java
@@ -0,0 +1,31 @@
+package com.example.restaurant.naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchLocalRes {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private String category;
+    private List<SearchLocalItem> items;
+
+
+    public static class SearchLocalItem {
+        private String title;
+        private String link;
+        private String description;
+        private String telephone;
+        private String address;
+        private String roadAddress;
+        private int mapx;
+        private int mapy;
+    }
+}
