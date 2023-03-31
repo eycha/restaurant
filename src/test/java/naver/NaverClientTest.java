7b40c3174e526ed03d0c31a9f7fdecbfdfa4c798
eycha <silvlexonst@gmail.com>
2023-03-31 19:07:03 +0900

    test: naver client test

---
+++ src/test/java/naver/NaverClientTest.java
@@ -0,0 +1,23 @@
+package naver;
+
+import com.example.restaurant.naver.NaverClient;
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NaverClientTest {
+    @Autowired
+    private NaverClient naverClient;
+
+    @Test
+    public void localSearchTest() {
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        var result = naverClient.localSearch(search);
+        System.out.println(result);
+
+    }
+}
